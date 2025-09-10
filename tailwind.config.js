6dd839b746832680a3d182fcbd652fdd125fe59c
conshipai <thomas.ehler@conship.ai>
2025-09-10 09:40:34 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'conship-purple': '#5B21B6',
+        'conship-orange': '#FB923C',
+      }
+    }
+  },
+  plugins: []
+}
